8e32cc3fc3d7fcf21fc57771b907c6dccd2e85d6
Casperfeng <casp.feng@gmail.com>
2019-04-10 17:50:48 +0200

    Complete assignment 1

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
  const url = 'https://api.darksky.net/forecast/125df3fc227fb25bf7898d5f7982af96/37.8267,-122.4233';
 
- request({ url: url }, (error, response) => {
-   const data = JSON.parse(response.body);
-   console.log(data.currently);
+ request({ url: url, json: true }, (error, response) => {
+   console.log(`It is currently ${response.body.currently.temperature} degrees out. There is a ${response.body.currently.precipProbability} chance of rain`);
  });
+
