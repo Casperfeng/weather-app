d9902cb63d62ced829c8b6c65ec7e500ff22973a
Casperfeng <casp.feng@gmail.com>
2019-04-10 17:13:06 +0200

    Add output of current weather data

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
  const request = require('request');
 
- 
+ const url = 'https://api.darksky.net/forecast/125df3fc227fb25bf7898d5f7982af96/37.8267,-122.4233';
+
+ request({ url: url }, (error, response) => {
+   const data = JSON.parse(response.body);
+   console.log(data.currently);
+ });
