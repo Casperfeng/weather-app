382a2dd05ae2e8e13dbb45647cc8784c66aa3f85
Casperfeng <casp.feng@gmail.com>
2019-04-10 17:54:34 +0200

    Display celsius instead of fahrenheit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
  const request = require('request');
 
- const url = 'https://api.darksky.net/forecast/125df3fc227fb25bf7898d5f7982af96/37.8267,-122.4233';
+ const url = 'https://api.darksky.net/forecast/125df3fc227fb25bf7898d5f7982af96/37.8267,-122.4233?units=si';
 
  request({ url: url, json: true }, (error, response) => {
    console.log(`It is currently ${response.body.currently.temperature} degrees out. There is a ${response.body.currently.precipProbability} chance of rain`);
