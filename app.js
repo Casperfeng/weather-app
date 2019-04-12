b217772874fed3c7801e98d313673ccd613300b5
Casperfeng <casp.feng@gmail.com>
2019-04-12 13:17:50 +0200

    Split app.js into utils

--- app.js
+++ app.js
@@ -1,24 +1,7 @@
 const request = require('request');
-
-const getUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiY2FzcGVyZiIsImEiOiJjanViZzl6cnYwMjRjNDRwOWhvankxZ2FyIn0.a-Tikw0Q0sgQY-xXN0Eh_Q&limit=1';
-
-const geocode = (adress, callback) => {
-  const url =  'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(adress) + '.json?access_token=pk.eyJ1IjoiY2FzcGVyZiIsImEiOiJjanViZzl6cnYwMjRjNDRwOWhvankxZ2FyIn0.a-Tikw0Q0sgQY-xXN0Eh_Q&limit=1';
-
-  request( {url: url, json: true}, (error, response) => {
-    if (error) {
-      callback('Unable to connect to location services', undefined);
-    } else if (response.body.features.length === 0) {
-      callback('Unable to  find location. Try another search', undefined);
-    } else {
-      callback(undefined, {
-        latitude: response.body.features[0].center[0],
-        longitude: response.body.features[0].center[1],
-      });
-    }
-  })
-};
+const geocode = require('./utils/geocode');
 
 geocode('Trondheim', (error, data) => {
-    console.log(data);
+  console.log('Error', error)
+  console.log('Data', data)
 });
